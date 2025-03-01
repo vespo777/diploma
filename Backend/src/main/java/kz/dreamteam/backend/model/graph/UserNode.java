15b042ccb08c3334a2d8483571fd8552b341757b
vespo777 <alibay.tileukhan@gmail.com>
2025-03-01 19:37:14 +0500

    ml + graph partially

---
+++ Backend/src/main/java/kz/dreamteam/backend/model/graph/UserNode.java
@@ -0,0 +1,135 @@
+package kz.dreamteam.backend.model.graph;
+
+import java.util.List;
+
+public class UserNode {
+    public int id;
+    public int age;
+    public List<String> interests;
+    public String regionFrom;
+    public List<String> languages;
+    public String religion;
+    public String petsStatus;
+    public int sleepTime;
+    public int budgetMax;
+    public int personalityType;
+    public String profession;
+    public boolean drinking;
+    public boolean smoking;
+    public String universityName;
+
+    // Конструкторы, геттеры и сеттеры
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
+    }
+
+    public String getRegionFrom() {
+        return regionFrom;
+    }
+
+    public void setRegionFrom(String regionFrom) {
+        this.regionFrom = regionFrom;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<String> languages) {
+        this.languages = languages;
+    }
+
+    public String getReligion() {
+        return religion;
+    }
+
+    public void setReligion(String religion) {
+        this.religion = religion;
+    }
+
+    public String getPetsStatus() {
+        return petsStatus;
+    }
+
+    public void setPetsStatus(String petsStatus) {
+        this.petsStatus = petsStatus;
+    }
+
+    public int getSleepTime() {
+        return sleepTime;
+    }
+
+    public void setSleepTime(int sleepTime) {
+        this.sleepTime = sleepTime;
+    }
+
+    public int getBudgetMax() {
+        return budgetMax;
+    }
+
+    public void setBudgetMax(int budgetMax) {
+        this.budgetMax = budgetMax;
+    }
+
+    public int getPersonalityType() {
+        return personalityType;
+    }
+
+    public void setPersonalityType(int personalityType) {
+        this.personalityType = personalityType;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public boolean isDrinking() {
+        return drinking;
+    }
+
+    public void setDrinking(boolean drinking) {
+        this.drinking = drinking;
+    }
+
+    public boolean isSmoking() {
+        return smoking;
+    }
+
+    public void setSmoking(boolean smoking) {
+        this.smoking = smoking;
+    }
+
+    public String getUniversityName() {
+        return universityName;
+    }
+
+    public void setUniversityName(String universityName) {
+        this.universityName = universityName;
+    }
+}
+
--- Backend/src/main/java/kz/dreamteam/backend/repository/UserRepository.java
+++ Backend/src/main/java/kz/dreamteam/backend/repository/UserRepository.java
@@ -3,10 +3,13 @@ package kz.dreamteam.backend.repository;
 import kz.dreamteam.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    List<User> findAllByOrderByIdAsc();
 }
 
