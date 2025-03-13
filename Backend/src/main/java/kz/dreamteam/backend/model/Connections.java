7437248c252aab36574dceffcfee001c5b9dc992
vespo777 <alibay.tileukhan@gmail.com>
2025-03-13 19:50:34 +0500

    connections working version

---
+++ Backend/src/main/java/kz/dreamteam/backend/model/Connections.java
@@ -0,0 +1,50 @@
+package kz.dreamteam.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Connections {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id") // Первичный ключ такой же, как в User
+    private Long id;
+
+    private Long senderId;
+    private Long receiverId;
+    private String status;
+
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(Long senderId) {
+        this.senderId = senderId;
+    }
+
+    public Long getReceiverId() {
+        return receiverId;
+    }
+
+    public void setReceiverId(Long receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
